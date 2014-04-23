cd3c7a592204879e0c359c428f5d790431048eb0
AndriyBas <avikbascra@gmail.com>
2014-04-23 22:27:49 +0300

    add Common dir with sample Book project

---
+++ study/Common/T12-Book/src/com/commonsware/empublite/ContentsAdapter.java
@@ -0,0 +1,29 @@
+package com.commonsware.empublite;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class ContentsAdapter extends FragmentStatePagerAdapter {
+  private BookContents contents=null;
+
+  public ContentsAdapter(FragmentActivity ctxt,
+                         BookContents contents) {
+    super(ctxt.getSupportFragmentManager());
+
+    this.contents=contents;
+  }
+
+  @Override
+  public Fragment getItem(int position) {
+    String path=contents.getChapterFile(position);
+
+    return(SimpleContentFragment.newInstance("file:///android_asset/book/"
+        + path));
+  }
+
+  @Override
+  public int getCount() {
+    return(contents.getChapterCount());
+  }
+}
